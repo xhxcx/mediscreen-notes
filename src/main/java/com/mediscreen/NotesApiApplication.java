f77ff8e862f02637317c5de64a9aac3f841352a0
xhxcx <xxhxcxx.sn@gmail.com>
2021-11-23 14:53:09 +0100

    initial commit with springboot conf

---
+++ src/main/java/com/mediscreen/NotesApiApplication.java
@@ -0,0 +1,11 @@
+package com.mediscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotesApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mediscreen/NotesApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mediscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NotesApiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
